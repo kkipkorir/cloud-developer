2c9d44f7d792aabc6097b1d43c38c0866c2f6224
kkipkorir <kipkoronsum@gmail.com>
2022-06-18 15:48:35 +0300

    Finished up the image-filter-udacity project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -12,24 +12,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+ 
+  //gets an image from a public url and filters it and returns the result
+  app.get("/filteredimage", async ( req, res) => {
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //get Image url from the request
+  let {  image_url: imageUrl } = req.query;
 
-  /**************************************************************************** */
+  //handles the case where the image url is not provided
+  if (!imageUrl) {
+    return res.status(400).send('Provide an image url');
+  }
 
-  //! END @TODO1
+  // send the resulting file in the response
+  // deletes any files on the server on finish of the response
+try{
+  let imageFilterResult = await filterImageFromURL(imageUrl)
+
+  return res.status(200).sendFile(imageFilterResult, (f) => {
+     deleteLocalFiles([imageFilterResult])
+   });
+  }
+
+  // Handles the case where link is the broken
+  catch(error){
+   return  res.status(422).send("Unable to download image");
+  }
+     
+});
   
   // Root Endpoint
   // Displays a simple message to the user
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,7 @@
+import { response } from "express";
 import fs from "fs";
 import Jimp = require("jimp");
+import axios ,{AxiosResponse} from "axios";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,7 +13,12 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const imageDetails: AxiosResponse = await axios({
+        method: "get",
+        url: inputURL,
+        responseType: "arraybuffer",
+      });
+      const photo = await Jimp.read(imageDetails.data);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
